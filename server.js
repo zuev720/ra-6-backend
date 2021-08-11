04e83a34d467899d488b20f2e437cc4892b21ab6
zuev720 <zuev720mail.ru>
2021-08-11 11:57:22 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({json: true}));
+
+const notes = [{id: 0, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'}, {id: 1, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'}];
+// let nextId = 1;
+
+const router = new Router();
+
+router.get('/notes', async (ctx, next) => {
+    ctx.response.body = notes;
+});
+
+router.post('/notes', async(ctx) => {
+    notes.push(ctx.request.body);
+    ctx.response.status = 204;
+});
+
+router.delete('/notes/:id', async(ctx, next) => {
+    const noteId = ctx.params.id;
+    const index = notes.findIndex(o => o.id.toString() === noteId);
+    if (index !== -1) {
+        notes.splice(index, 1);
+    }
+    ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7777;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('server started'));
